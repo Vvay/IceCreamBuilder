a8b65f7dd89547e8e52615c01e6e5657be1a8fcb
Alex <ola6552@gmail.com>
2022-05-13 22:54:18 +0200

    Main implementation with additional use

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Main {
+import builders.IceCreamBuilder;
+import builders.IngredientsBuilder;
+import director.Director;
+import models.IceCream;
+import models.Ingredients;
+
+public class Main {
+    // MENU
+    // 1. Oreo Standard  (S) - Creamy with oreo sprinkles
+    // 2. Choco surprise (S) - Strawberry with chocolate glaze and coconut flakes
+    // 3. Sweet madness  (S) - Mixed with toffee topping and fruits
+    // 4. Eve's dream    (S) - Creamy with apple glaze and gold sprinkles
+    // 5. Fruity Fall    (L) - Mixed with whipped cream and fruits
+
+    public static void main(String[] args) {
+        Director director = new Director();
+        IngredientsBuilder ingredientsBuilder = new IngredientsBuilder();
+        director.constructSweetMadness(ingredientsBuilder);
+        Ingredients ingredients = ingredientsBuilder.getResult();
+        System.out.println(ingredients.toString());
+
+        IceCreamBuilder builder = new IceCreamBuilder();
+        director.constructChocoSurprise(builder);
+        IceCream iceCream = builder.getResult();
+        System.out.println(iceCream.toString());
+    }
 }
